66305e847affe8ad866d4f7263858b4948a6749e
Michael Sumner <mdsumner@gmail.com>
2015-12-17 07:06:36 +1100

    Update writeGeo.R

--- R/writeGeo.R
+++ R/writeGeo.R
@@ -2,11 +2,13 @@ extent_plot <- function(usr) {
   extent(usr)
 }
 
+## something like this . . .
 writeGeo <- function(filename, device = dev.cur(), proj = NA_character_, ...) {
  tfile <- sprintf("%s.tif", tempfile())
  on.exit(unlink(tfile))
   savePlot(tfile, type = "tif", device = device)
-  r <- raster(tfile)
+  r <- brick(tfile)
+  r <- brick(r[[3]], r[[2]], r[[1]])
   extent(r) <-  extent_plot(par("usr"))
   projection(r) <- proj
   writeRaster(r, filename, ...)
